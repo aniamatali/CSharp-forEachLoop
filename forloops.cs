3e4ed0bc452d4aaf6be0fa85c289f8a2f30c2a9f
alvin niamatali <aniamatali@icloud.com>
2017-10-06 10:43:33 -0700

    uppercasing friends list

--- forloops.cs
+++ forloops.cs
@@ -1,24 +1,45 @@
 using System;
 
-class GroceryList
+class Friends
 {
   static void Main()
   {
-    string[] myGroceryList = {"eggs", "milk", "bread"};
-    int[] groceryListPrices = {3,6,4};
+    string[] myFriendsList = {"kurt", "cathy", "michael"};
 
-    Console.WriteLine("My grocery list:");
-    foreach (string groceryItem in myGroceryList)
+
+    Console.WriteLine("My friends list:");
+    foreach (string friend in myFriendsList)
     {
-      Console.WriteLine(groceryItem);
+      string upperString = friend.ToUpper();
+      Console.WriteLine(upperString);
     }
 
-    int total = 0;
-    foreach (int price in groceryListPrices)
-    {
-      total = total + price;
-    }
 
-    Console.WriteLine("Your total for this shopping trip is $"+ total);
+
   }
 }
+
+// using System;
+//
+// class GroceryList
+// {
+//   static void Main()
+//   {
+//     string[] myGroceryList = {"eggs", "milk", "bread"};
+//     int[] groceryListPrices = {3,6,4};
+//
+//     Console.WriteLine("My grocery list:");
+//     foreach (string groceryItem in myGroceryList)
+//     {
+//       Console.WriteLine(groceryItem);
+//     }
+//
+//     int total = 0;
+//     foreach (int price in groceryListPrices)
+//     {
+//       total = total + price;
+//     }
+//
+//     Console.WriteLine("Your total for this shopping trip is $"+ total);
+//   }
+// }
